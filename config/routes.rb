d566da13a6459189a39cfb7f4cbb75deacbcf01a
lewis nyoike <nyoikelewis@gmail.com>
2017-11-06 11:52:17 +0300

    Added resource for about us page

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :projects
   resources :home
   resources :contact_us
+  resources :about_us
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
