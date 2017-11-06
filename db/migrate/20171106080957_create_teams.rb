a67cd8b3b0d6e51c31b2b9c64288475ccdad248a
lewis nyoike <nyoikelewis@gmail.com>
2017-11-06 11:15:53 +0300

    Scaffold for the teams part

---
+++ db/migrate/20171106080957_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.column :postion, :varchar
+      t.column :statement, :varchar
+
+      t.timestamps
+    end
+  end
+end
