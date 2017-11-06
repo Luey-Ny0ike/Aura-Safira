29571aa911fb9f2ada920a6067ada4cdc6d0eb85
lewis nyoike <nyoikelewis@gmail.com>
2017-11-06 11:16:20 +0300

    Fixed the role issue

--- db/migrate/20171106080957_create_teams.rb
+++ db/migrate/20171106080957_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration[5.1]
     create_table :teams do |t|
       t.string :name
       t.column :postion, :varchar
+      t.column :role, :varchar
       t.column :statement, :varchar
 
       t.timestamps
