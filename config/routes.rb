c43756031fd870d3927b19de44bfb723580ff26d
lewis nyoike <nyoikelewis@gmail.com>
2017-11-07 12:42:11 +0300

    Removed all traces of the contact_us thing

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :teams
   resources :projects
   resources :home
-  resources :contact_us
   resources :about_us
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
