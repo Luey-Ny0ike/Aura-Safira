876a634d9a0eb56756c2cfb6c022276229991456
lewis nyoike <nyoikelewis@gmail.com>
2017-11-08 12:52:28 +0300

    Permitted the avatar

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -69,6 +69,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:name, :postion, :role, :statement)
+      params.require(:team).permit(:name, :postion, :role, :statement, :avatar)
     end
 end
