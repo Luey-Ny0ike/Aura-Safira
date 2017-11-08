d1d61c0f3f913f40ca5b9b26187d890e84a71ea8
lewis nyoike <nyoikelewis@gmail.com>
2017-11-08 12:24:17 +0300

    Mounted the rails_admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
   root to: "home#index"
   resources :contacts
   resources :blogs do
