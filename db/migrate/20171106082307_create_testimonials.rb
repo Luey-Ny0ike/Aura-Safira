a0a370d92a05ffd676f52a2d3cda8a8ab5ac3153
Lewis Nyoike <nyoikelewis@gmail.com>
2017-12-17 14:45:26 +0300

    Removed initial testimonial migrations

--- db/migrate/20171106082307_create_testimonials.rb
+++
@@ -1,9 +0,0 @@
-class CreateTestimonials < ActiveRecord::Migration[5.1]
-  def change
-    create_table :testimonials do |t|
-      t.string :testimony
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20171204075512_add_name_and_position_to_testimonials.rb
+++
@@ -1,6 +0,0 @@
-class AddNameAndPositionToTestimonials < ActiveRecord::Migration[5.1]
-  def change
-    add_column :testimonials, :name, :string
-    add_column :testimonials, :position, :string
-  end
-end
